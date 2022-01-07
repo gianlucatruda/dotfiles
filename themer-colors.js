255c2a12545e784ef524100a36eedd21841faea7
Gianluca Truda <gianlucatruda@gmail.com>
2022-01-07 17:31:44 +0100

    Custom colour themes (themer) config file.

---
+++ themer-colors.js
@@ -0,0 +1,30 @@
+// This file can be used with the themer CLI, see https://github.com/themerdev/themer
+
+module.exports.colors = {
+  "dark": {
+    "shade0": "#1E1E1E",
+    "shade7": "#FEFFFE",
+    "accent0": "#FF5257",
+    "accent1": "#F7821C",
+    "accent2": "#FBB927",
+    "accent3": "#62BA46",
+    "accent4": "#00C7A3",
+    "accent5": "#007AFF",
+    "accent6": "#A550A7",
+    "accent7": "#F74F9E"
+  },
+  "light": {
+    "shade0": "#EBEBEB",
+    "shade7": "#393939",
+    "accent0": "#E0383E",
+    "accent1": "#F7821C",
+    "accent2": "#FBB927",
+    "accent3": "#62BA46",
+    "accent4": "#10B9A6",
+    "accent5": "#007AFF",
+    "accent6": "#953D96",
+    "accent7": "#F74F9E"
+  }
+};
+
+// Your theme's URL: https://themer.dev/?colors.dark.shade0=%231e1e1e&colors.dark.shade7=%23fefffe&colors.dark.accent0=%23FF5257&colors.dark.accent1=%23F7821C&colors.dark.accent2=%23FBB927&colors.dark.accent3=%2362BA46&colors.dark.accent4=%2300C7A3&colors.dark.accent5=%23007AFF&colors.dark.accent6=%23A550A7&colors.dark.accent7=%23F74F9E&colors.light.shade0=%23ebebeb&colors.light.shade7=%23393939&colors.light.accent0=%23E0383E&colors.light.accent1=%23F7821C&colors.light.accent2=%23FBB927&colors.light.accent3=%2362BA46&colors.light.accent4=%2310B9A6&colors.light.accent5=%23007AFF&colors.light.accent6=%23953D96&colors.light.accent7=%23F74F9E&activeColorSet=dark&calculateIntermediaryShades.dark=true&calculateIntermediaryShades.light=true
