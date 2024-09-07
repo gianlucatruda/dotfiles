a8f3bcf7493d6efd0d784774a82352ceefd7f13d
Gianluca Truda <dev@gianluca.ai>
2024-09-07 17:12:06 +0100

    nvim: <Leader>f formats current buffer with LSP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -573,6 +573,7 @@ local on_attach = function(_, bufnr)
   nmap('<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
+  nmap('<leader>f', vim.lsp.buf.format, '[F]ormat current buffer')
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
