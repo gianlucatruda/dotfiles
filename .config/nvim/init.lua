925a2fe0a9a53f7995d8b1b15f603d7a6b088d4d
Gianluca Truda <dev@gianluca.ai>
2024-10-15 19:47:12 +0100

    nvim plugin: dingllm with OpenAI support.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -298,6 +298,38 @@ require('lazy').setup({
     },
     opts = {},
   },
+  {
+    'yacineMTB/dingllm.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local system_prompt =
+      'You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks'
+      local helpful_prompt = 'You are a helpful assistant. What I have sent are my notes so far.'
+      local dingllm = require 'dingllm'
+
+      local function openai_replace()
+        dingllm.invoke_llm_and_stream_into_editor({
+          url = 'https://api.openai.com/v1/chat/completions',
+          model = 'gpt-4o',
+          api_key_name = 'OPENAI_API_KEY',
+          system_prompt = system_prompt,
+          replace = true,
+        }, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
+      end
+
+      local function openai_help()
+        dingllm.invoke_llm_and_stream_into_editor({
+          url = 'https://api.openai.com/v1/chat/completions',
+          model = 'gpt-4o',
+          api_key_name = 'OPENAI_API_KEY',
+          system_prompt = helpful_prompt,
+          replace = false,
+        }, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
+      end
+      vim.keymap.set({ 'n', 'v' }, '<leader>L', openai_help, { desc = '[L]LM OpenAI (helpful)' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>l', openai_replace, { desc = '[L]LM OpenAI (terse)' })
+    end,
+  },
 
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
