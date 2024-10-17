bf9d5b308d37d2b3a5c54670ed35f2e9cb76943b
Gianluca Truda <dev@gianluca.ai>
2024-10-17 02:44:06 +0100

    Refactored plugin structure. Nighfox theme.

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -23,47 +23,27 @@ require("lazy").setup({
 	-- modular approach: using `require 'path/name'` will
 	-- include a plugin definition from file lua/path/name.lua
 
-	require("kickstart/plugins/gitsigns"),
+	require("plugins/gitsigns"),
 
-	require("kickstart/plugins/which-key"),
+	require("plugins/which-key"),
 
-	require("kickstart/plugins/telescope"),
+	require("plugins/telescope"),
 
-	require("kickstart/plugins/lspconfig"),
+	require("plugins/lspconfig"),
 
-	require("kickstart/plugins/conform"),
+	require("plugins/conform"),
 
-	require("kickstart/plugins/cmp"),
+	require("plugins/cmp"),
 
-	require("kickstart/plugins/tokyonight"),
+	-- require("plugins/tokyonight"),
 
-	require("kickstart/plugins/todo-comments"),
+	require("plugins/todo-comments"),
 
-	require("kickstart/plugins/mini"),
+	require("plugins/mini"),
 
-	require("kickstart/plugins/treesitter"),
+	require("plugins/treesitter"),
 
-	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-	-- init.lua. If you want these files, they are in the repository, so you can just download them and
-	-- place them in the correct locations.
-
-	-- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-	--
-	--  Here are some example plugins that I've included in the Kickstart repository.
-	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
-	--
-	-- require 'kickstart.plugins.debug',
-	-- require 'kickstart.plugins.indent_line',
-	-- require 'kickstart.plugins.lint',
-	-- require 'kickstart.plugins.autopairs',
-	-- require 'kickstart.plugins.neo-tree',
-
-	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-	--    This is the easiest way to modularize your config.
-	--
-	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-	--    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-	{ import = "custom.plugins" },
+	require("plugins/nightfox"),
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
