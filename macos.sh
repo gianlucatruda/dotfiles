fc8ece3c602b98e29ba82897958ad2734940e4d1
Gianluca Truda <dev@gianluca.ai>
2025-01-20 19:05:51 +0000

    Updated macos.sh script to group mission control by app (to gel with Aerospace).

--- macos.sh
+++ macos.sh
@@ -238,9 +238,8 @@ defaults write com.apple.dock launchanim -bool false
 # Speed up Mission Control animations
 defaults write com.apple.dock expose-animation-duration -float 0.1
 
-# Don’t group windows by application in Mission Control
-# (i.e. use the old Exposé behavior instead)
-defaults write com.apple.dock expose-group-by-app -bool false
+# Group windows by application in Mission Control (works better with Aerospace)
+defaults write com.apple.dock expose-group-apps -bool true
 
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
