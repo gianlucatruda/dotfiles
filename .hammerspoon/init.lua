91075858f551bc9ad33fabd6417a0a2af28a7693
Gianluca Truda <dev@gianluca.ai>
2025-01-20 17:10:04 +0000

    Updated karabiner and hammerspoon: right_cmd -> hyper as main modifier key

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,16 +1,19 @@
--- Quick switch to favourite apps with cmd+<N>
-hs.hotkey.bind({ "cmd" }, "1", function()
+-- Hyper = left_command + left_shift + left_option + left_control (set in Karabiner)
+local hyper = { "cmd", "shift", "alt", "ctrl" }
+
+-- Quick switch to favourite apps with Hyper+<N>
+hs.hotkey.bind(hyper, "1", function()
 	hs.application.launchOrFocus("Firefox")
 end)
-hs.hotkey.bind({ "cmd" }, "2", function()
+hs.hotkey.bind(hyper, "2", function()
 	hs.application.launchOrFocus("Alacritty")
 end)
-hs.hotkey.bind({ "cmd" }, "3", function()
+hs.hotkey.bind(hyper, "3", function()
 	hs.application.launchOrFocus("Obsidian")
 end)
-hs.hotkey.bind({ "cmd" }, "4", function()
+hs.hotkey.bind(hyper, "4", function()
 	hs.application.launchOrFocus("Calendar")
 end)
-hs.hotkey.bind({ "cmd" }, "5", function()
-	hs.application.launchOrFocus("Spotify.app")
+hs.hotkey.bind(hyper, "5", function()
+	hs.application.launchOrFocus("Spotify")
 end)
