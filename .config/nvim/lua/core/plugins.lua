c0ba20c153749ca47f55ef0a8199a0b90531b042
Gianluca Truda <dev@gianluca.ai>
2025-05-08 18:56:12 +0100

    Refactor Neovim plugin configuration
    Consolidated and simplified the Neovim plugins configuration by removing nested structures and redundant comments. This refactoring streamlines the setup, making it more maintainable and easier to read while retaining the existing functionalities and dependencies.

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -1,102 +1,45 @@
 require('lazy').setup({
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-  -- Useful plugin to show you pending keybinds.
-  'folke/which-key.nvim',
-  {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
-      -- Useful status updates for LSP
-      'j-hui/fidget.nvim',
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
-    },
-  },
-  {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build Step is needed for regex support in snippets
-          -- This step is not supported in many windows environments
-          -- Remove the below condition to re-enable on windows
-          if vim.fn.has 'win32' == 1 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-      },
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-    },
-  },
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-  {
-    'EdenEast/nightfox.nvim',
-    priority = 1000,
-    lazy = false,
-  },
+  'tpope/vim-fugitive',    -- Git related plugin
+  'tpope/vim-rhubarb',     -- Git related plugins
+  'tpope/vim-sleuth',      -- Detect tabstop and shiftwidth automatically
+  'folke/which-key.nvim',  -- Useful plugin to show you pending keybinds.
+  'neovim/nvim-lspconfig', -- LSP support
+  { 'williamboman/mason.nvim', config = true },
+  'williamboman/mason-lspconfig.nvim',
+  -- Useful status updates for LSP
+  'j-hui/fidget.nvim',
+  -- Additional lua configuration, makes nvim stuff amazing!
+  'folke/neodev.nvim',
+  -- Autocompletion
+  'hrsh7th/nvim-cmp',
+  -- Snippets
+  'L3MON4D3/LuaSnip',
+  'saadparwaiz1/cmp_luasnip',
+  -- Adds LSP completion capabilities
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-path',
+  -- Adds a number of user-friendly snippets
+  'rafamadriz/friendly-snippets',
+  'lewis6991/gitsigns.nvim',
+  -- Nightfox colourscheme
+  { 'EdenEast/nightfox.nvim',  priority = 1000, lazy = false, },
   -- Set lualine as statusline
   'nvim-lualine/lualine.nvim',
-
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- See `:help ibl`
-    main = 'ibl',
-  },
-
-  -- "gc" to comment visual regions/lines
+  -- Add indentation guides even on blank lines
+  { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', },
+  -- Use "gc" to toggle comment on visual regions/lines
   'numToStr/Comment.nvim',
-
   -- Fuzzy Finder (files, lsp, etc)
+  'nvim-lua/plenary.nvim',
   {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-    },
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+    cond = function() return vim.fn.executable 'make' == 1 end,
   },
-
-  {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-  },
-
+  { 'nvim-telescope/telescope.nvim',       branch = '0.1.x', },
+  -- Highlight, edit, and navigate code
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate', },
   -- Centerpad a single buffer
   'smithbm2316/centerpad.nvim',
-
 }, {})
