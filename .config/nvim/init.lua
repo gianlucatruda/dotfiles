d4eb0f3032b686e04bf231b408a7fe321a8de462
Gianluca Truda <dev@gianluca.ai>
2025-05-08 16:57:09 +0100

    Modularised keymaps section to core.keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,37 +4,13 @@
 -- Start KEYMAPS --------------------------------------------------------------
 -- See `:help vim.keymap.set()`
 
--- Set <space> as the leader key
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
--- Visual mode: move selected lines up and down with J and K (i.e. shift+j and shift+k)
--- (adapted from ThePrimeagen: https://www.youtube.com/watch?v=w7i4amO_zaE)
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
--- Insert current date in yyyy-mm-dd format
-vim.keymap.set('n', '<leader>id', function()
-  vim.cmd("r!date +\\%F")
-end, { desc = '[I]nsert current [D]ate' })
--- Centerpad the buffer
-vim.keymap.set('n', '<leader>z', '<cmd>Centerpad<cr>')
-
--- ???
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+require("core.keymaps") -- Goes to lua/core/keymaps.lua
 
 -- End KEYMAPS ----------------------------------------------------------------
 
 -- Start PREFS ----------------------------------------------------------------
+--
+-- TODO include same settings as .vimrc here
 
 -- Gianluca's custom tab settings (vim-sleuth will override in some contexts?) -----
 -- Note: these are similar to my defaults in .vimrc for vanilla vim (good baseline)
