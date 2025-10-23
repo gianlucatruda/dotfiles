65b2b028dea7826d20c92ca976a06f904e927dd2
Gianluca Truda <dev@gianluca.ai>
2025-10-23 14:10:11 +0100

    FEAT: Only apply nightfox theme to nvim if in alacritty

--- .config/nvim/lua/plugin_config/colourscheme.lua
+++ .config/nvim/lua/plugin_config/colourscheme.lua
@@ -1,10 +1,22 @@
-require('nightfox').setup({
-  options = {
-    compile_path = vim.fn.stdpath("cache") .. "/nightfox", -- Compilation
-    compil_file_suffix = "_compiled",                      -- Cache compiled themes
-    module_default = false,                                -- Disable unused themes
-  },
-})
+local function detect_alacritty()
+  -- Figure out if running in alacritty (to apply same colourscheme)
+  if vim.env.ALACRITTY_LOG or vim.env.ALACRITTY_SOCKET then
+    return true
+  end
+  return false
+end
 
-vim.cmd("colorscheme nightfox")
-require('nightfox').load()
+local running_in_alacritty = detect_alacritty()
+
+if running_in_alacritty then
+  require('nightfox').setup({
+    options = {
+      compile_path = vim.fn.stdpath("cache") .. "/nightfox", -- Compilation
+      compil_file_suffix = "_compiled",                      -- Cache compiled themes
+      module_default = false,                                -- Disable unused themes
+    },
+  })
+
+  vim.cmd("colorscheme nightfox")
+  require('nightfox').load()
+end
