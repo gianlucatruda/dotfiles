4b7b0865851d6165f15dfd933662e294ae9fb3c6
Gianluca Truda <dev@gianluca.ai>
2026-01-17 16:35:09 +0000

    Reload nvim files after lazygit closes
    - Add TermClose autocmd matching "*lazygit*" to run vim.cmd("checktime")
    - Ensures buffers update when files are changed via lazygit
    - Keeps existing <leader>lg mapping to open LazyGit

--- .config/nvim/lua/plugin_config/lazygit.lua
+++ .config/nvim/lua/plugin_config/lazygit.lua
@@ -1,2 +1,10 @@
 -- Lazygit integration
 vim.keymap.set('n', '<leader>lg', '<cmd>LazyGit<cr>', { desc = 'Open [L]azy[G]it' })
+
+-- Auto-reload files after lazygit closes
+vim.api.nvim_create_autocmd("TermClose", {
+  pattern = "*lazygit*",
+  callback = function()
+    vim.cmd("checktime")
+  end,
+})
