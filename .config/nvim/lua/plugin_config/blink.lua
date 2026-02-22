133bd891d07df426254ff4ddce69aeef97e8444c
Gianluca Truda <dev@gianluca.ai>
2026-02-22 15:39:13 +0000

    nvim: Enhanced blink completions

--- .config/nvim/lua/plugin_config/blink.lua
+++ .config/nvim/lua/plugin_config/blink.lua
@@ -12,9 +12,24 @@ require('blink.cmp').setup {
     ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
     ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
   },
+  completion = {
+    menu = {
+      draw = {
+        columns = {
+          { 'label', 'label_description', gap = 1 },
+          { 'kind' },
+        },
+      },
+    },
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 250,
+    },
+  },
   snippets = { preset = 'luasnip' },
   sources = {
     -- Add 'buffer' if you want word completion from open buffers.
     default = { 'lsp', 'path', 'snippets' },
   },
+  signature = { enabled = true },
 }
